175e49b078a9df83875f20996d98b50a738a711f
cjd1110 <cjd1110@koreatech.ac.kr>
2018-10-23 13:12:31 +0900

    header, footer 생성
    - 공통 컴포넌트 header, footer 생성
    - angular version 7 update
    
    close #11

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  type: string = 'lol';
+
+  constructor(private cdr : ChangeDetectorRef) {
+
+  }
+
+  setType(type) {
+    this.type = type;
+    this.cdr.detectChanges();
+  }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,20 +1,47 @@
 import { NgModule } from '@angular/core';
 import {TopNavComponent} from './components/top-nav/top-nav.component';
-import {MatIconModule, MatToolbarModule} from '@angular/material';
+import {
+  MatChipsModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule, MatListModule, MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {CommonModule} from '@angular/common';
+import { BottomComponent } from './components/bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    TopNavComponent
+    TopNavComponent,
+    BottomComponent
   ],
   imports: [
+    CommonModule,
     MatToolbarModule,
+    MatButtonToggleModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatListModule,
   ],
   providers: [],
   exports: [
     TopNavComponent,
+    BottomComponent,
     MatToolbarModule,
-    MatIconModule
+    MatButtonToggleModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatListModule,
   ]
 })
 export class SharedModule { }
