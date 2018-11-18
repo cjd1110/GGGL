071f77eb63cdf833b37df057265a3b13d58918ea
cjd1110 <cjd1110@koreatech.ac.kr>
2018-11-18 23:24:07 +0900

    search rxjs 기능 분리 및 성능 개선 (#38)
    - 사용자 아이디 검색시 존재 하지 않을시 추천 리스트 초기화 완료
    - keyup$ 기능 과 users$ 기능 분리 완료
    - retry 함수를 사용하여 재도전 후 실패시 사용자에 alert창으로 경고 표시
    
    refs #37

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {FormControl} from '@angular/forms';
 
-import {fromEvent} from 'rxjs';
-import {debounceTime, distinctUntilChanged, filter, map, mergeMap, tap} from 'rxjs/operators';
-import {FormControl} from "@angular/forms";
+import {
+  debounceTime,
+  distinctUntilChanged, finalize,
+  partition, retry,
+  switchMap,
+  tap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -39,19 +44,40 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     // github id search로 angular & rxjs sample code
-    const users$ = this.searchControl.valueChanges
+    const keyup$ = this.searchControl.valueChanges
       .pipe(
         debounceTime(300), // 300ms 뒤에 데이터 전달
         distinctUntilChanged(), // 특수키가 입력된 경우에는 나오지 않기 위해 중복 데이터 처리
-        filter(query => query.trim().length > 0),
-        tap(this.showLoading),
-        mergeMap(query => this.http.get(`https://api.github.com/search/users?q=${query}`)),
-        tap(this.hideLoading)
       );
 
-    users$.subscribe(res => {
-      this.users = res['items'];
+    // 단순히 query만 사용할 경우 type error 가 발생함 Object를 string으로 변환 후 사용
+    // 처음 글자 입력시 length가 3부터 시작이라 -2 함
+    // partition 함수가 rxjs 에서 문제가 발생하는 경우가 많아 폐지 예정이라고 함
+    // https://github.com/ReactiveX/rxjs/issues/2995
+    let [users$, reset$] = partition(query => {
+      return JSON.stringify(query).trim().length - 2 > 0;
+    })(keyup$);
+
+    users$ = users$
+      .pipe(
+        tap(this.showLoading),
+        switchMap(query => this.http.get(`https://api.github.com/search/users?q=${query}`)),
+        tap(this.hideLoading),
+        retry(2),
+        finalize(this.hideLoading)
+      );
+
+    users$.subscribe({
+      next: value => this.users = value['items'],
+      error: error => {
+        console.error(error);
+        alert(error.message);
+      }
     });
+
+    reset$.pipe(
+      tap(v => this.users = [])
+    ).subscribe();
   }
 
   showLoading() {
