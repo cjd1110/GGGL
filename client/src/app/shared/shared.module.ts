8ad60cdd2867cd417282a6780005affa82707ded
유학현 <yu@yuhaghyeon-ui-MacBook-Pro.local>
2018-11-18 17:32:05 +0900

    git id 조회하는 search 기능 구현
    - angular 와 rxjs를 활용하여 search 기능 구현
    - github id를 HttpClient 객체 활용하여 get 방식으로 실제 데이터 조회
    - autocomplete 기능을 활용하여, 추천 아이디ui로 노출
    
    refs #33

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
-import {HeaderComponent} from './components/header/header.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {CommonModule} from '@angular/common';
 import {
+  MatAutocompleteModule,
   MatCardModule,
   MatChipsModule,
   MatDividerModule,
@@ -9,8 +12,8 @@ import {
   MatInputModule, MatListModule, MatTabsModule,
   MatToolbarModule
 } from '@angular/material';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {CommonModule} from '@angular/common';
+
+import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 
 @NgModule({
@@ -30,6 +33,9 @@ import {FooterComponent} from './components/footer/footer.component';
     MatTabsModule,
     MatListModule,
     MatCardModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   exports: [
@@ -45,12 +51,9 @@ import {FooterComponent} from './components/footer/footer.component';
     MatTabsModule,
     MatListModule,
     MatCardModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
-export class SharedModule {
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: SharedModule,
-    }
-  }
-}
+export class SharedModule {}
