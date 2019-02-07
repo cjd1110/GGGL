9dcffee58b25e1c32f260cefaabb81a34e4ee56a
cjd1110 <cjd1110@koreatech.ac.kr>
2019-02-08 01:09:58 +0900

    android webView fileupload 추가
    - webView fileupload example 추가
    - angular fileupload example 추가
    - rxjs 임시제거
    
    refs #43

--- client/src/app/switch/switch-news/switch-news.component.ts
+++ client/src/app/switch/switch-news/switch-news.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./switch-news.component.scss']
 })
 export class SwitchNewsComponent implements OnInit {
-
+  public flag: boolean;
   constructor() { }
 
   ngOnInit() {
   }
 
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    if(fileList.length > 0) {
+      let file: File = fileList[0];
+      this.flag = true;
+      console.log('file ', fileList);
+
+
+      // let formData:FormData = new FormData();
+      // formData.append('uploadFile', file, file.name);
+      // let headers = new Headers();
+      /** In Angular 5, including the header Content-Type can invalidate your request */
+      // headers.append('Content-Type', 'multipart/form-data');
+      // headers.append('Accept', 'application/json');
+      // let options = new RequestOptions({ headers: headers });
+      // this.http.post(`${this.apiEndPoint}`, formData, options)
+      //   .map(res => res.json())
+      //   .catch(error => Observable.throw(error))
+      //   .subscribe(
+      //     data => console.log('success'),
+      //     error => console.log(error)
+      //   )
+    }
+  }
 }
